5d2f0cc44f47db85b5015ebff6204c1da6763f62
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2023-01-02 16:05:12 +0100

    write build_tree

---
+++ main.rb
@@ -0,0 +1,38 @@
+class Node
+  attr_accessor :right, :left, :data
+
+  def initialize(data)
+    @data = data
+    @right = nil
+    @left = nil
+  end
+end
+
+class Tree
+  attr_accessor :root
+
+  def initialize(arr)
+    @arr = arr.uniq.sort
+    @root = build_tree(arr)
+  end
+
+  def build_tree(arr = @arr)
+    return nil if arr.empty?
+
+    mid = (arr.length - 1) / 2
+    root = Node.new(arr[mid])
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[mid + 1..])
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
+
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.pretty_print
